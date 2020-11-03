b20fdd03c79960907e049b7b2e0c305f222d9c60
ErzaFoxy <ErzaFoxy55565@gmail.com>
2020-11-03 14:53:36 +0200

    fix and improve catalog section, make cards with js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,195 @@
+let catalogArrayFirst = [{
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Polar FT1',
+        'descr': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '4 750 руб.',
+        'price': '4 500 руб.'
+    },
+    {
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Suunto M2',
+        'descr': 'Пульсометр начального уровня с комфортным тканевым ремешком',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '6 990 руб.',
+        'price': '6 641 руб.'
+    },
+    {
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Polar FT1',
+        'descr': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '4 750 руб.',
+        'price': '4 500 руб.'
+    },
+    {
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Polar FT1',
+        'descr': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '4 750 руб.',
+        'price': '4 500 руб.'
+    },
+    {
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Suunto M2',
+        'descr': 'Пульсометр начального уровня с комфортным тканевым ремешком',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '6 990 руб.',
+        'price': '6 641 руб.'
+    },
+    {
+        'image': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle': 'Пульсометр Polar FT4',
+        'descr': 'Улучшенная версия Polar FT1, имеющая следующие улучшения:',
+        'list': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice': '7 390 руб.',
+        'price': '7 021 руб.'
+    }
+];
+
+let catalogArraySecond = [{
+    'image_2': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+    'subtitle_2': 'Пульсометр Polar FT1',
+    'descr_2': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+    'list_2': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+        '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+        '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+        '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+    'oldPrice_2': '4 750 руб.',
+    'price_2': '4 500 руб.'
+}];
+
+let catalogArrayThird = [{
+        'image_3': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle_3': 'Пульсометр Polar FT1',
+        'descr_3': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+        'list_3': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice_3': '4 750 руб.',
+        'price_3': '4 500 руб.'
+    },
+    {
+        'image_3': '<img src="images/photo_wristwatch.jpg" alt="pusometr" class="catalog-item__img">',
+        'subtitle_3': 'Пульсометр Polar FT1',
+        'descr_3': 'Для первых шагов в тренировках, основанных на сердечном ритме',
+        'list_3': '<li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>' +
+            '<li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>' +
+            '<li>Также Вы увидите информацию о расходе калорий за тренировку;</li>' +
+            '<li>Вы сможете посмотреть данные по 10 тренировкам.</li>',
+        'oldPrice_3': '4 750 руб.',
+        'price_3': '4 500 руб.'
+    }
+];
+
+const catalogItemFirst = ({ image, subtitle, descr, list, oldPrice, price }) => {
+    return ` 
+<div class="catalog-item">
+<div class="catalog-item__wrapper">
+    <div class="catalog-item__content catalog-item__content_active">${image}
+        <div class="catalog-item__subtitle">${subtitle}</div>
+        <div class="catalog-item__descr">${descr}</div>
+        <a href="" class="catalog-item__link">ПОДРОБНЕЕ</a>
+    </div>
+    <ul class="catalog-item__list">${list}
+    <a href="#" class="catalog-item__back">назад</a>
+    </ul>
+</div>
+<hr>
+
+<div class="catalog-item__footer">
+    <div class="catalog-item__prices">
+        <div class="catalog-item__old-price">${oldPrice}</div>
+        <div class="catalog-item__price">${price}</div>
+    </div>
+    <button class="button button_mini">КУПИТЬ</button>
+</div>
+</div>
+`
+};
+
+const catalogItemSecond = ({ image_2, subtitle_2, descr_2, list_2, oldPrice_2, price_2 }) => {
+    return ` 
+<div class="catalog-item">
+<div class="catalog-item__wrapper">
+    <div class="catalog-item__content catalog-item__content_active">${image_2}
+        <div class="catalog-item__subtitle">${subtitle_2}</div>
+        <div class="catalog-item__descr">${descr_2}</div>
+        <a href="" class="catalog-item__link">ПОДРОБНЕЕ</a>
+    </div>
+    <ul class="catalog-item__list">${list_2}
+    <a href="#" class="catalog-item__back">назад</a>
+    </ul>
+</div>
+<hr>
+
+<div class="catalog-item__footer">
+    <div class="catalog-item__prices">
+        <div class="catalog-item__old-price">${oldPrice_2}</div>
+        <div class="catalog-item__price">${price_2}</div>
+    </div>
+    <button class="button button_mini">КУПИТЬ</button>
+</div>
+</div>
+`
+};
+
+const catalogItemThird = ({ image_3, subtitle_3, descr_3, list_3, oldPrice_3, price_3 }) => {
+    return ` 
+<div class="catalog-item">
+<div class="catalog-item__wrapper">
+    <div class="catalog-item__content catalog-item__content_active">${image_3}
+        <div class="catalog-item__subtitle">${subtitle_3}</div>
+        <div class="catalog-item__descr">${descr_3}</div>
+        <a href="" class="catalog-item__link">ПОДРОБНЕЕ</a>
+    </div>
+    <ul class="catalog-item__list">${list_3}
+    <a href="#" class="catalog-item__back">назад</a>
+    </ul>
+</div>
+<hr>
+
+<div class="catalog-item__footer">
+    <div class="catalog-item__prices">
+        <div class="catalog-item__old-price">${oldPrice_3}</div>
+        <div class="catalog-item__price">${price_3}</div>
+    </div>
+    <button class="button button_mini">КУПИТЬ</button>
+</div>
+</div>
+`
+};
+
+
 $(document).ready(function() {
+    $('#catalog-item_1').prepend(catalogArrayFirst.map(catalogItemFirst).join(' '));
+    $('#catalog-item_2').prepend(catalogArraySecond.map(catalogItemSecond).join(' '));
+    $('#catalog-item_3').prepend(catalogArrayThird.map(catalogItemThird).join(' '));
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/icon_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/icon_right.png"></button>',
         responsive: [{
             breakpoint: 992,
             settings: {
