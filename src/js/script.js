4420e3d6c6850d600a7e7276d7eb8a49680409c5
ErzaFoxy <ErzaFoxy55565@gmail.com>
2020-11-06 23:19:18 +0200

    add ajax for forms, add modals, add mask for input

--- src/js/script.js
+++ src/js/script.js
@@ -179,4 +179,62 @@ $(document).ready(function() {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn("slow");
+    });
+    $('.modals__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut("slow");
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modals__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn("slow");
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask('+38 (999) 999-99-99');
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
